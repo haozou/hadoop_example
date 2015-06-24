0e19ffde5a80a8a5eaffbf242d9ec93176d5704d
Hao Zou <hao@alpinenow.com>
2015-06-24 09:33:08 -0700

    uncomment

--- avro_example/src/main/java/com/alpine/runner/App.java
+++ avro_example/src/main/java/com/alpine/runner/App.java
@@ -29,42 +29,42 @@ import java.util.List;
 public class App {
     public void run(String[] args) throws IOException, ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchMethodException, InvocationTargetException, URISyntaxException {
         System.out.println(findJar());
-//        ClassLoader classLoader = App.class.getClassLoader();
-//        Class clazz = classLoader.loadClass("com.alpine.runner.App");
-//        System.out.println("com.alpine".split("\\.")[0]);
-//
-//        String name = "com.alpine.runner.App";
-//        String[] names = name.split("\\.");
-//        System.out.println(name.indexOf(names[names.length - 1]));
-//        System.out.println(name.substring(0, name.indexOf(names[names.length - 1]) - 1));
-//        Class testClass = clazz.getClasses()[0];
-//        System.out.println(testClass.getCanonicalName());
-//        Object testd = testClass.newInstance();
-//        Method test = testd.getClass().getDeclaredMethod("test");
-//        test.setAccessible(true);
-//        test.invoke(testd);
-//        //System.out.println(test.getName() + "!!!!!!!!!");
-//        // Deserialize users from disk
-//        FsInput fsInput = null;
-//        Configuration conf = new Configuration();
-//        conf.set("fs.default.name", "hdfs://10.0.0.146:8020");
-//        //File file = new File("StudentActivity.snappy.avro");
-//        Path p = new Path("hdfs://10.0.0.146:8020/Datasets/avro/StudentActivity.snappy.avro");
-//        fsInput = new FsInput(p, conf);
-//        //System.out.println(file.getAbsolutePath());
-//        DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
-//        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(fsInput, datumReader);
-//        GenericRecord user = null;
-//        while (dataFileReader.hasNext()) {
-//// Reuse user object by passing it to next(). This saves us from
-//// allocating and garbage collecting many objects for files with
-//// many items.
-//            user = dataFileReader.next(user);
-//            int size = user.getSchema().getFields().size();
-//            for (int i = 0; i < size; i++)
-//                System.out.print(user.get(i).toString() + ",");
-//            System.out.println();
-//        }
+        ClassLoader classLoader = App.class.getClassLoader();
+        Class clazz = classLoader.loadClass("com.alpine.runner.App");
+        System.out.println("com.alpine".split("\\.")[0]);
+
+        String name = "com.alpine.runner.App";
+        String[] names = name.split("\\.");
+        System.out.println(name.indexOf(names[names.length - 1]));
+        System.out.println(name.substring(0, name.indexOf(names[names.length - 1]) - 1));
+        Class testClass = clazz.getClasses()[0];
+        System.out.println(testClass.getCanonicalName());
+        Object testd = testClass.newInstance();
+        Method test = testd.getClass().getDeclaredMethod("test");
+        test.setAccessible(true);
+        test.invoke(testd);
+        //System.out.println(test.getName() + "!!!!!!!!!");
+        // Deserialize users from disk
+        FsInput fsInput = null;
+        Configuration conf = new Configuration();
+        conf.set("fs.default.name", "hdfs://10.0.0.146:8020");
+        //File file = new File("StudentActivity.snappy.avro");
+        Path p = new Path("hdfs://10.0.0.146:8020/Datasets/avro/StudentActivity.snappy.avro");
+        fsInput = new FsInput(p, conf);
+        //System.out.println(file.getAbsolutePath());
+        DatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>();
+        DataFileReader<GenericRecord> dataFileReader = new DataFileReader<GenericRecord>(fsInput, datumReader);
+        GenericRecord user = null;
+        while (dataFileReader.hasNext()) {
+// Reuse user object by passing it to next(). This saves us from
+// allocating and garbage collecting many objects for files with
+// many items.
+            user = dataFileReader.next(user);
+            int size = user.getSchema().getFields().size();
+            for (int i = 0; i < size; i++)
+                System.out.print(user.get(i).toString() + ",");
+            System.out.println();
+        }
     }
     public static void main(final String[] args) throws IOException, InterruptedException {
 
