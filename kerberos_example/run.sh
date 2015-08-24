2f3f9bf0cc155e63d85ac01450eb151d48b4a43a
Hao Zou <hao@alpinenow.com>
2015-08-24 15:54:13 -0700

    commit for example

--- kerberos_example/run.sh
+++ kerberos_example/run.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 mvn clean
 mvn package
-mvn exec:java -Djava.security.krb5.realm=ALPINE -Djava.security.krb5.kdc=alpinenode4.alpinenow.local  -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.alpine.runner.KerberosExample" -Dexec.args="-Dnode1=cdh5hakerberosnn.alpinenow.local acro_sample testoutput"
+mvn exec:java -Dnamenode=cdh5hakerberosnn.alpinenow.local -Dmetastore=cdh5hakerberosnn.alpinenow.local -Djava.security.krb5.realm=ALPINE -Djava.security.krb5.kdc=alpinenode4.alpinenow.local  -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.alpine.runner.KerberosExample" -Dexec.args="chorus/chorus.alpinenow.local@ALPINE ./chorus.keytab hao"
 
-#mvn exec:java -Dexec.cleanupDaemonThreads=false -Dexec.mainClass="com.alpine.runner.HCatalogExample" -Dexec.args="default.acro_sample"
 
 
--- kerberos_example/src/main/java/com/alpine/runner/KerberosExample.java
+++ kerberos_example/src/main/java/com/alpine/runner/KerberosExample.java
@@ -1,14 +1,14 @@
 package com.alpine.runner;
 
-import com.alpine.utility.Utilities;
 import com.facebook.fb303.FacebookBase;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hive.conf.HiveConf;
 import org.apache.hadoop.hive.metastore.HiveMetaStore;
-import org.apache.hadoop.hive.shims.Hadoop23Shims;
+import org.apache.hadoop.security.SaslRpcServer;
 import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
@@ -24,29 +24,30 @@ import java.util.List;
  * Created by Hao on 8/19/15.
  */
 public class KerberosExample extends Configured implements Tool {
-    public static final List<Class> libjars = new ArrayList<Class>() {{
-        add(HCatRecord.class); //hcatalog-core
-        //add(HCatClient.class); //hcatalog-webclient
-        //add(HiveEndPoint.class);
-        //add(HCatEventMessage.class);
-        add(HiveMetaStore.class);
-        add(EnumMetaData.class);
-        //add(AbstractMapJoinOperator.class);
-        add(FacebookBase.class);
-    }};
+    private static final String HIVE_METASTORE_URIS = "hive.metastore.uris";
+    private static final String HIVE_METASTORE_SASL_ENABLED = "hive.metastore.sasl.enabled";
+    private static final String HIVE_METASTORE_KERBEROS_PRINCIPAL = "hive.metastore.kerberos.principal";
+    private static final String HIVE_METASTORE_LOCAL = "hive.metastore.local";
+    private static final String HADOOP_RPC_PROTECTION = "hadoop.rpc.protection";
 
     public static void main(final String[] args) throws Exception {
-        final String[] hadoopArgs = {"-libjars", Utilities.libjars(libjars)};
         final Configuration conf = new Configuration();
-        conf.set("hive.metastore.sasl.enabled", "true");
-        conf.set("hive.server2.enable.impersonation", "true");
-
-        UserGroupInformation ugi = UserGroupInformation.loginUserFromKeytabAndReturnUGI("chorus/chorus.alpinenow.local@ALPINE", "/Users/Hao/workspace/hadoop_example/kerberos_example/chorus.keytab");
-        //ugi = UserGroupInformation.createProxyUser("hive", ugi);
+        UserGroupInformation.setConfiguration(conf);
+        if (args.length < 3) {
+            System.err.println("no principle, keytab or proxyuser provided");
+            System.exit(1);
+        }
+        String principle = args[0];
+        String keytab = args[1];
+        String proxyUser = args[2];
+        System.out.println("principle: " + principle + ", keytab: " + keytab + ", proxyUser: " + proxyUser);
+        UserGroupInformation.loginUserFromKeytab(principle, keytab);
+        UserGroupInformation ugi = UserGroupInformation.createProxyUser(proxyUser, UserGroupInformation.getLoginUser());
+        //ugi = UserGroupInformation.getLoginUser();
         ugi.doAs(new PrivilegedExceptionAction<KerberosExample>() {
             public KerberosExample run() throws Exception {
                 KerberosExample mr = new KerberosExample();
-                ToolRunner.run(conf, mr, (String[]) ArrayUtils.addAll(hadoopArgs, args));
+                ToolRunner.run(conf, mr, args);
                 return mr;
             }
         });
@@ -57,12 +58,26 @@ public class KerberosExample extends Configured implements Tool {
     public int run(String[] args) throws Exception {
         Configuration conf = getConf();
         org.apache.hadoop.fs.FileSystem fs = org.apache.hadoop.fs.FileSystem.get(conf);
+        System.out.println("hdfs root directory:");
         for (FileStatus fileStatus : fs.listStatus(new Path("/"))) {
             System.err.println(fileStatus.getPath().getName());
         }
-        /*HCatClient example code start*/
-        HCatClient client = HCatClient.create(conf);
-        for (String name : client.listTableNamesByPattern(null, "*")) {
+
+        /*hcatalog example*/
+        HiveConf hiveConf = null;
+        HCatClient hiveclient = null;
+        hiveConf = new HiveConf();
+        // specified a thrift url
+        hiveConf.set(HIVE_METASTORE_SASL_ENABLED, "true");
+        hiveConf.set(HIVE_METASTORE_KERBEROS_PRINCIPAL, "hive/_HOST@"+System.getProperty("java.security.krb5.realm"));
+        String protection = conf.get(HADOOP_RPC_PROTECTION,
+                SaslRpcServer.QualityOfProtection.AUTHENTICATION.name()
+                        .toLowerCase());
+        hiveConf.set(HADOOP_RPC_PROTECTION, protection);
+
+        hiveclient = HCatClient.create(hiveConf);
+        System.out.println("hive default database:");
+        for (String name : hiveclient.listTableNamesByPattern(null, "*")) {
             System.err.println(name);
         }
         return 0;
